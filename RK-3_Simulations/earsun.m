c93fa78581d10a2cfd8145170bacb88bdb4b851a
Shade5 <f2013112@goa.bits-pilani.ac.in>
2016-09-03 10:13:27 +0530

    actual Diatances

--- RK-3_Simulations/earsun.m
+++ RK-3_Simulations/earsun.m
@@ -8,7 +8,7 @@ function [E,Ev,B,Bv] = earsun(xe,ve,xs,vs)
     Ev=zeros(3661,3);
     Bv=zeros(3661,3);
     
-    timerange=0:6*24*60:60*24*60*366;
+    timerange=0:6*24*60:60*24*60*366*3;
     
     initial=[xe(1),xe(2),xe(3),ve(1),ve(2),ve(3),xs(1),xs(2),xs(3),vs(1),vs(2),vs(3)];
     
